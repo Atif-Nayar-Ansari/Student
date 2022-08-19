9d71a34e97ad64042d6127a2ec25cd77c37b5e4d
Atif-Nayar-Ansari <atifnayaransari999@gmail.com>
2022-08-20 01:25:14 +0530

    Navigation highlight, proper page navigation dropdown :stable

--- src/main/java/com/student/controller/ContactController.java
+++ src/main/java/com/student/controller/ContactController.java
@@ -4,27 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by abraun on 10/11/2017.
- */
 @Controller
-public class DemoController {
-
+public class ContactController {
 
     @ModelAttribute("subpage")
     public String navbarActive() {
-        return "home-active";
+        return "contact-active";
     }
 
-    @RequestMapping(value="/")
-    public String home() {
-        return "home";
+    @RequestMapping(value="/contact")
+    public String contact() {
+        return "contact";
     }
-
-    @RequestMapping(value="/about")
-    public String about() {
-        return "about";
-    }
-
 
 }
--- src/main/java/com/student/controller/course/StudentAjaxController.java
+++ src/main/java/com/student/controller/course/StudentAjaxController.java
@@ -3,16 +3,17 @@ package com.student.controller.course;
 import com.student.enities.course.StudentDetailsEntity;
 import com.student.services.course.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class StudentAjaxController {
 
     @Autowired
     private StudentService studentService;
 
-    @GetMapping("/saveStudent")
+    @GetMapping("/save-one-student")
     public void saveStudent(){
         StudentDetailsEntity studentDetailsEntity = new StudentDetailsEntity();
         studentDetailsEntity.setStudentName("Ryan");
